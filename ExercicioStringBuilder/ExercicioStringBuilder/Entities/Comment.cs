7797fc103cc27bd49d712d137b5dbe6adbf822e3
Gilberto Savisky <saviskydeveloper@gmail.com>
2019-03-19 11:10:20 -0300

    Exemplo de StringBuilder

---
+++ ExercicioStringBuilder/ExercicioStringBuilder/Entities/Comment.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExercicioStringBuilder.Entities
+{
+    class Comment
+    {
+        public string Text { get; set; }
+
+        public Comment()
+        {
+        }
+
+        public Comment(string text)
+        {
+            Text = text;
+        }
+
+
+    }
+}
