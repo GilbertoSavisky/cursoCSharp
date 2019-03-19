978d4a49146b9464eb0f1a7d7e39a87826068fa0
Gilberto Savisky <saviskydeveloper@gmail.com>
2019-03-19 10:00:22 -0300

    Exercicio de retangulos

---
+++ Retangulo/Retangulo/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExercicioRetangulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Retangulo ret = new Retangulo();
+            Console.WriteLine("Entre com a largura e altura do retângulo:");
+            Console.Write("Largura: ");
+            ret.Largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Altura: ");
+            ret.Altura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine();
+            Console.WriteLine($"Área do retângulo: {ret.Area()}");
+
+            Console.WriteLine();
+            Console.WriteLine($"Perímetro do retângulo: {ret.Perimetro()}");
+
+            Console.WriteLine();
+            Console.WriteLine($"Diagonal do retângulo: {ret.Diagonal()}");
+        }
+    }
+}
---
+++ Retangulo/Retangulo/Retangulo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ExercicioRetangulo
+{
+    class Retangulo
+    {
+        public double Largura, Altura;
+
+        public double Area()
+        {
+            return Largura * Altura;
+        }
+
+        public double Perimetro()
+        {
+            return (Altura * 2) + (Largura * 2);
+        }
+
+        public double Diagonal()
+        {
+            double x = (Altura * Altura) + (Largura * Largura);
+
+            return Math.Sqrt(x);
+        }
+    }
+}
