682fee239744523e1d6cb6acb6361573eef2b3c7
Gilberto Savisky <saviskydeveloper@gmail.com>
2019-03-19 09:52:00 -0300

    Exemplo Aluno Notas

---
+++ ExAlunoNotas/ExAlunoNotas/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExercicioAluno
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Aluno aluno = new Aluno();
+            Console.WriteLine("Entre com os dados do aluno: ");
+            Console.Write("Nome: ");
+            aluno.Nome = Console.ReadLine();
+
+            Console.Write("Nota 1: ");
+            aluno.Nota1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Nota 2: ");
+            aluno.Nota2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Nota 3: ");
+            aluno.Nota3 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            aluno.Situacao();
+        }
+    }
+}
