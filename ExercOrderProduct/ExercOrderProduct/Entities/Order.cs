ded10e07e17639d102d6507993999e415b76f439
Gilberto Savisky <saviskydeveloper@gmail.com>
2019-03-19 11:32:22 -0300

    Order Product, classes criadas

---
+++ ExercOrderProduct/ExercOrderProduct/Entities/Order.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using ExercOrderProduct.Entities.Enums;
+
+namespace ExercOrderProduct.Entities
+{
+    class Order
+    {
+        public DateTime Moment { get; set; }
+        public OrderStatus Status { get; set; }
+        public List<OrderItem> Items { get; set; } = new List<OrderItem>();
+        public List<Client> Client { get; set; }
+
+        public Order()
+        { }
+
+        public Order(DateTime moment, OrderStatus status)
+        {
+            Moment = moment;
+            Status = status;
+        }
+
+        public void AddItem(OrderItem item)
+        {
+            Items.Add(item);
+        }
+
+        public void RemoveItem(OrderItem item)
+        {
+            Items.Remove(item);
+        }
+
+        public double Total()
+        {
+            double sum = 0;
+            foreach(OrderItem item in Items)
+            {
+                sum += item.Price;
+            }
+            return sum;
+        }
+    }
+}
---
+++ ExercOrderProduct/ExercOrderProduct/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExercOrderProduct
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
