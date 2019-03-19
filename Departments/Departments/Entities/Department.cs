0c3da076936a47f96d2cfc5f4e7a62f1b141ea83
Gilberto Savisky <saviskydeveloper@gmail.com>
2019-03-19 09:03:35 -0300

    first commit

---
+++ Departments/Departments/Entities/Department.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Departments.Entities
+{
+    class Department
+    {
+        public string Name { get; set; }
+
+        public Department()
+        {
+
+        }
+
+        public Department(string name)
+        {
+            Name = name;
+        }
+    }
+    
+}
---
+++ TipoEnums/TipoEnums/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using TipoEnums.Entities;
+using TipoEnums.Entities.Enums;
+
+namespace TipoEnums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Order order = new Order
+            {
+                Id = 1080,
+                Moment = DateTime.Now,
+                Status = OrderStatus.PedingPayment
+            };
+
+            Console.WriteLine(order);
+        }
+    }
+}
