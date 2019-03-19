c1038c8b59c52e62c837f4631e5c4846d496c56d
Gilberto Savisky <saviskydeveloper@gmail.com>
2019-03-19 10:12:41 -0300

    Aluguel de quartos de Estudantes

---
+++ PensaoEstudantes/PensaoEstudantes/Estudantes.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PensaoEstudantes
+{
+    class Estudantes
+    {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Nome}, {Email}";
+        }
+    }
+    
+}
