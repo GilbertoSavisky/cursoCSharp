d509364e8d8dd1333296fcf2a6a2dc754efd2a3c
Gilberto Savisky <savisky@developer@gmail.com>
2019-04-01 09:52:11 -0300

    Exercicio de fixacao - Exceptions

---
+++ Account_ExcFixacao/Account_ExcFixacao/Entities/Exceptions/DominioException.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Account_ExcFixacao.Entities.Exceptions
+{
+    class DominioException : Exception
+    {
+        public DominioException(string mensagem) :  base(mensagem)
+        { }
+    }
+}
