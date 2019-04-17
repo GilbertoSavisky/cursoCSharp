45731b00cbe145ba35fbea518cc66746892172ca
Gilberto Savisky <savisky@developer@gmail.com>
2019-04-17 10:06:19 -0300

    Classe Comparison<T> - Demo1

---
+++ ClasseComparison/ClasseComparison/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace System
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Produto> list = new List<Produto>();
+
+            list.Add(new Produto("TV", 900.00));
+            list.Add(new Produto("Notebook", 1200.00));
+            list.Add(new Produto("Tablet", 450.00));
+
+            list.Sort();
+
+            foreach(Produto p in list)
+                Console.WriteLine(p);
+        }
+    }
+}
