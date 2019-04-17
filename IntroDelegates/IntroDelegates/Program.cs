dec74a4f3a269d777bd60de7d42298f2704d44ca
Gilberto Savisky <savisky@developer@gmail.com>
2019-04-17 10:46:10 -0300

    Intro a Delegates

---
+++ IntroDelegates/IntroDelegates/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace System
+{
+    class Program
+    {
+        //BinaryNumericOperation
+        delegate double OperacaoBinariaNumerica(double n1, double n2);
+
+        static void Main(string[] args)
+        {
+            double a = 10;
+            double b = 12;
+
+            OperacaoBinariaNumerica op = CalculationService.Sum;
+
+            double result = op(a, b);
+            Console.WriteLine(result);
+        }
+    }
+}
