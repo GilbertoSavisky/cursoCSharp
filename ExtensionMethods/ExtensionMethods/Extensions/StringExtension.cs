08c817535d27799c3c7d5b2c9ab1ff630e6f558a
Gilberto Savisky <savisky@developer@gmail.com>
2019-04-17 09:39:36 -0300

    Extension Methods - Demo2

---
+++ ExtensionMethods/ExtensionMethods/Extensions/StringExtension.cs
@@ -0,0 +1,13 @@
+﻿namespace System
+{ 
+    static class StringExtension
+    {
+        public static string Cut(this string thisObjt, int count)
+        {
+            if (thisObjt.Length <= count)
+                return thisObjt;
+            else
+                return thisObjt.Substring(0, count) + "...";
+        }
+    }
+}
--- ExtensionMethods/ExtensionMethods/Program.cs
+++ ExtensionMethods/ExtensionMethods/Program.cs
@@ -1,12 +1,11 @@
-﻿
-using System;
+﻿using System;
 
-    class Program
+class Program
+{
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            DateTime dt = new DateTime(2019, 04, 17, 8, 10, 45);
-            Console.WriteLine(dt.ElapsedTime());
-        }
+        string s1 = "Good morning dear students!";
+        Console.WriteLine(s1.Cut(10));
     }
+}
 
