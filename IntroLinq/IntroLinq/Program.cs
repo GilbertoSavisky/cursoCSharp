4c53fdb46740a7e3b7f99cb6356aba0ab9ba48b3
Gilberto Savisky <gilbertosavisky@gmail.com>
2019-04-23 15:21:12 -0300

    LINQ com notacao similar a SQL

--- IntroLinq/IntroLinq/Program.cs
+++ IntroLinq/IntroLinq/Program.cs
@@ -39,53 +39,32 @@ namespace System
                 new Produto() {Id= 11, Nome = "Nivel", Preco = 70.0, Categoria = c1}
             };
 
-            var r1 = produtos.Where(p => p.Categoria.Categorias == 1 && p.Preco < 900);
+            //var r1 = produtos.Where(p => p.Categoria.Categorias == 1 && p.Preco < 900);
+            var r1 = from p in produtos where p.Categoria.Categorias == 1 && p.Preco < 900 select p;
             Print("Categoria 1 e preco < 900: ", r1);
 
-            var r2 = produtos.Where(p => p.Categoria.Nome == "Ferramentas").Select(p => p.Nome);
+            //var r2 = produtos.Where(p => p.Categoria.Nome == "Ferramentas").Select(p => p.Nome);
+            var r2 = from p in produtos where p.Categoria.Nome == "Ferramentas" select p.Nome;
             Print("Nomes de produtos da Cat ferramentas", r2);
 
-            var r3 = produtos.Where(p => p.Nome[0] == 'C').Select(p => new { p.Nome, p.Preco, CatNome = p.Categoria.Nome});
+            //var r3 = produtos.Where(p => p.Nome[0] == 'C').Select(p => new { p.Nome, p.Preco, CatNome = p.Categoria.Nome});
+            var r3 = from p in produtos where p.Nome[0] == 'C' select new { p.Nome, p.Preco, CatName = p.Categoria.Nome };
             Print("Nomes começados com 'C' e obj anonimos", r3);
 
-            var r4 = produtos.Where(p => p.Categoria.Categorias == 1).OrderBy(p => p.Preco).ThenBy(p => p.Nome);
+            //var r4 = produtos.Where(p => p.Categoria.Categorias == 1).OrderBy(p => p.Preco).ThenBy(p => p.Nome);
+            var r4 = from p in produtos where p.Categoria.Categorias == 1 orderby p.Nome orderby p.Preco select p;
             Print("Cat 1 order by preço then nome", r4);
 
-            var r5 = r4.Skip(2).Take(4); // skipe(pula) 2 e take(pega) 4 elementos
+            //var r5 = r4.Skip(2).Take(4); // skipe(pula) 2 e take(pega) 4 elementos
+            var r5 = (from p in r4 select p).Skip(2).Take(4);
             Print("Cat 1 order by preço then nome skipe 2 e take 4", r5);
 
-            var r6 = produtos.First();
-            Console.WriteLine("Primeiro elemento: \n\t"+r6);
-
-            var r7 = produtos.Where(p => p.Preco > 3000).FirstOrDefault();
-            Console.WriteLine("Primeiro elemento ou default: \n\t" + r7);
-
             Console.WriteLine();
 
-            var r8 = produtos.Where(p => p.Id == 3).SingleOrDefault();
-            Console.WriteLine("Single or default: \n\t"+ r8);
-            var r9 = produtos.Where(p => p.Id == 30).SingleOrDefault();
-            Console.WriteLine("Single or default: \n\t" + r9);
 
-            var r10 = produtos.Max(p => p.Preco);
-            Console.WriteLine("Preço maximo: "+r10);
-            var r11 = produtos.Min(p => p.Preco);
-            Console.WriteLine("Preço minimo: " + r11);
-
-            var r12 = produtos.Where(p => p.Categoria.Id == 1).Sum(p => p.Preco);
-            Console.WriteLine("Cat 1 com soma dos preços: "+r12);
-            var r13 = produtos.Where(p => p.Categoria.Id == 1).Average(p => p.Preco);
-            Console.WriteLine("Cat 1 com média dos preços: " + r13);
-
-            var r14 = produtos.Where(p => p.Categoria.Id == 5).Select(p => p.Preco).DefaultIfEmpty(0.0).Average();
-            Console.WriteLine("Cat 5 com média dos preços: " + r14);
-
-            var r15 = produtos.Where(p => p.Categoria.Id == 1).Select(p => p.Preco).Aggregate(0.0,(x, y) => x + y);
-            Console.WriteLine("Cat 1 aggregate soma :"+r15);
-
-            Console.WriteLine();
-            var r16 = produtos.GroupBy(p => p.Categoria);
-            foreach(IGrouping<Categoria, Produto> group in r16)
+            //var r16 = produtos.GroupBy(p => p.Categoria);
+            var r16 = from p in produtos group p by p.Categoria;
+            foreach (IGrouping<Categoria, Produto> group in r16)
             {
                 Console.WriteLine("Cat "+ group.Key.Nome+":");
                 foreach(Produto p in group)
